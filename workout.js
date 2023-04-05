4fb989009c1e07620ca9485b72e24cd415fabbd7
twdinn <twdinn@gmail.com>
2023-04-05 15:29:19 -0230

    By Type

--- workout.js
+++ workout.js
@@ -34,6 +34,27 @@ class Exercise {
       )
       .catch((err) => console.error(err));
   };
+
+  getExerciseByType = () => {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "031f90f2bamsh08f92af0c6fe80cp1d5a1bjsndaa3aea41ea9",
+        "X-RapidAPI-Host": "exercises-by-api-ninjas.p.rapidapi.com",
+      },
+    };
+
+    fetch(`${exerciseURL}?type=${this.muscle}`, options)
+      .then((response) => response.json())
+      .then((response) =>
+        console.log(
+          `${response[randomNumber(response)].name}, Sets: ${
+            this.sets
+          }, Reps: ${this.reps}`
+        )
+      )
+      .catch((err) => console.error(err));
+  };
   // exerciseDay = () => {
   //   dayOfWeek = new Date().getDay();
   //   let exercise1;
@@ -69,3 +90,4 @@ class Exercise {
 }
 
 const exercise = new Exercise("biceps", 3, 10).getExerciseByMuscle();
+const stretching = new Exercise("stretching", 3, 10).getExerciseByType();
